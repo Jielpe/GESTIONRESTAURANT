a6de6b641a7c21d4862fef6a6586ddb78f94f612
Its-my-right <brendan.hans@gmail.com>
2015-05-11 15:21:28 +0200

    modif globale 3

---
+++ GestionHotelGit/src/test/Exec.java
@@ -0,0 +1,18 @@
+package test;
+
+import dao.DAO;
+import dao.DAOFactory;
+import dao.IDAO;
+import dao.VilleDAO;
+
+public class Exec {
+
+	public static void main(String[] args) {
+		DAOFactory df = DAOFactory.getInstance();
+		IDAO daopays=df.getDAO(DAO.Ville);
+		System.out.println(daopays.getFromId(11));
+		
+
+	}
+
+}
