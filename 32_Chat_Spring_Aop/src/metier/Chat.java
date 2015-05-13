96d4875cad8f48dd01e2ce87e9c813538fe0ddf9
Cochet <jielpe99@gmail.com>
2015-05-13 11:54:07 +0200

    chat

--- 32_Chat_Spring_Aop/src/metier/Chat.java
+++ 32_Chat_Spring_Aop/src/metier/Chat.java
@@ -1,31 +1,44 @@
 package metier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Chat implements IChat {
 	
-	public List<String> lstMessages = null;
+	public List<String> lstMessages = new ArrayList<String>();
+	
+	public Chat() {
+		
+	}
+	
+	
+
+	public Chat(List<String> lstMessages) {
+		super();
+		this.lstMessages = lstMessages;
+	}
+
+
 
 	@Override
 	public void addMessage(String pMessage) {
-		// TODO Auto-generated method stub
+		this.lstMessages.add(pMessage);
 
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		this.lstMessages = new ArrayList<String>();
 
 	}
-	
-	/**
-	 * Print the message list
-	 * @return String
-	 */
-	public String toString()
-	{
-		return null;
-		
+
+
+
+	@Override
+	public String toString() {
+		return "Chat [lstMessages=" + lstMessages + "]";
 	}
+	
+	
 
 }
