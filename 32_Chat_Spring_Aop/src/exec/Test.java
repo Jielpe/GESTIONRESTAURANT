8476faafdcd8489565730274d420d49bf2d097de
Cochet <jielpe99@gmail.com>
2015-05-13 12:03:23 +0200

    Working app

--- 32_Chat_Spring_Aop/src/exec/Test.java
+++ 32_Chat_Spring_Aop/src/exec/Test.java
@@ -1,9 +1,21 @@
 package exec;
 
+import metier.IChat;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
+	private static ApplicationContext context ;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		context = new ClassPathXmlApplicationContext(
+				"ApplicationContext.xml");
+		IChat chat = (IChat) context.getBean("chat");
+		chat.addMessage("Hello");
+		System.out.println(chat.toString());
+		chat.clear();
+		System.out.println(chat.toString());
 
 	}
 
